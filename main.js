c5782441526b790d87d8a11357a2ef377036251d
lauanymillnitz-commits <lauany.millnitz@aluno.senai.br>
2026-04-15 09:58:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+// QUESTÃO 1
+// let vendido = [5, 3, 8, 2, 6];
+
+// for(let i =0; i > vendido.lenght; i++){
+//     console.log(vendidos[i]);
+// }
+// somarvendidos =5+2+8+3+6;
+// console.log("o total que foram vendidos " + somarvendidos)
+
+
+// QUESTÃO 2
+// let valor= 234;
+
+// for(let i =0; i > valor.lenght; i++){
+//    desconto= (valor * 10) /100;
+// }
+// console.log("o valor com o desconto é de " + desconto);
+
+
+// QUESTÃO 3
+// let cliente = { nome: "Maria", idade: 25, cidade: "Caxias do Sul" };
+
+// console.log("Cliente " + (cliente.nome) + " mora em " + (cliente.cidade));
+
+
+// QUESTÃO 4
+// const alunos=["Ana", "Bruno", "Carlos",
+// "Daniel", "Eva"];
+
+// console.log("primeira " + alunos[0])
+// console.log("ultima " + alunos[alunos.length -1])
+
+
+// QUESTÃO 5
+// const botao =documentElement
+// const resultado=[0]
+
+// function mudartexto("click" resultado){
+//     console.log("texto alterado com sucesso")
+// }
+// QUESTÃO 6
+// const numeros= [100, 250, 80, 320, 50, 600];
+
+// for (let i = 0; i >= numeros.length; i++) {
+//   if (numeros > [i]){
+//     console.log("os num maioes que 100 sao" + numeros)
+//   }
+// }
+
+
+//QUESTAO 7
+let vendido = [200, 150, 300, 50];
+
+for(let i =0; i > vendido.lenght; i++){
+    console.log(vendidos[i]);
+}
+somarvendidos =200+150+300+50;
+console.log("o total que foram vendidos " + somarvendidos + "reais");
